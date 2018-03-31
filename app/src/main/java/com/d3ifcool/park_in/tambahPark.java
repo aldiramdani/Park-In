6e98f910d3575fedff93a195f9f9ed4aade86ec7
AldiRamdani <aldiramdani8@gmail.com>
2018-03-31 21:41:38 +0700

    Another PT

--- app/src/main/java/com/d3ifcool/park_in/tambahPark.java
+++ app/src/main/java/com/d3ifcool/park_in/tambahPark.java
@@ -1,15 +1,61 @@
 package com.d3ifcool.park_in;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.provider.MediaStore;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class tambahPark extends AppCompatActivity {
+    public static SQLiteHelper sqLiteHelper;
+    ImageButton imageTempat;
+    Button button;
+
+    TextInputEditText txt_nama_tempat,txt_ket_tempat;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageTempat.setImageBitmap(bitmap);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_park);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        imageTempat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,0);
+            }
+        });
+
+
+
+
     }
+
+
 }
