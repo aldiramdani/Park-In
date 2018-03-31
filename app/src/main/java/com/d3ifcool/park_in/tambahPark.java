f8e06271eb144cd5d81476be36de0a7c2571cfe6
AldiRamdani <aldiramdani8@gmail.com>
2018-03-31 22:06:46 +0700

    Another PT

--- app/src/main/java/com/d3ifcool/park_in/tambahPark.java
+++ app/src/main/java/com/d3ifcool/park_in/tambahPark.java
@@ -23,12 +23,14 @@ import java.util.Date;
 
 
 public class tambahPark extends AppCompatActivity {
-    public static SQLiteHelper sqLiteHelper;
+//    public static SQLiteHelper sqLiteHelper;
     ImageButton imageTempat;
     Button button;
 
     TextInputEditText txt_nama_tempat,txt_ket_tempat;
-
+    String formattedDate,hours;
+    Date c = Calendar.getInstance().getTime();
+    int currentHours;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -41,6 +43,16 @@ public class tambahPark extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_park);
+//        sqLiteHelper = new SQLiteHelper(this, "ParkDB.sqlite", null, 1);
+//        sqLiteHelper.queryData("CREATE TABLE IF NOT EXISTS PARKIR (ID INTEGER PRIMARY KEY AUTOINCREMENT, namaTempat VARCHAR, keterangan VARCHAR, tanggal VARCHAR, jam VARCHAR, gambar BLOB)");
+        imageTempat = (ImageButton)findViewById(R.id.image_tempat);
+        button = (Button)findViewById(R.id.button);
+        txt_nama_tempat = (TextInputEditText)findViewById(R.id.txt_nama_tempat);
+        txt_ket_tempat = (TextInputEditText)findViewById(R.id.txt_ket_tempat);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+        formattedDate = dateFormat.format(c);
+        Calendar rightnow = Calendar.getInstance();
+        currentHours = rightnow.get(Calendar.HOUR_OF_DAY);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -52,10 +64,39 @@ public class tambahPark extends AppCompatActivity {
             }
         });
 
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                try{
+//                    sqLiteHelper.insertData(
+//                          txt_nama_tempat.getText().toString().trim(),
+//                            txt_ket_tempat.getText().toString().trim(),
+//                            formattedDate,
+//                            currentHours,
+//                            ImageViewToByte(imageTempat)
+//
+//                    );
+//
+//                    Toast.makeText(getApplicationContext(),"Berhasil Di Tambah",Toast.LENGTH_LONG);
+//                    txt_nama_tempat.setText("");
+//                    txt_ket_tempat.setText("");
+//                    imageTempat.setImageResource(R.drawable.image);
+//                }
+//                catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
 
 
 
     }
 
-
+//    public static byte[] ImageViewToByte(ImageButton image){
+//      Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//        bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+//      byte[] bytesArray = stream.toByteArray();
+//      return bytesArray;
+//    }
 }
