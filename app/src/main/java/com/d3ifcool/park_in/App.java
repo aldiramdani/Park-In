a1b55b2af0302a4c30169b85c3a58c06a48a1d2f
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2018-04-01 12:54:09 +0700

    setup realm

---
+++ app/src/main/java/com/d3ifcool/park_in/App.java
@@ -0,0 +1,13 @@
+package com.d3ifcool.park_in;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class App extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
