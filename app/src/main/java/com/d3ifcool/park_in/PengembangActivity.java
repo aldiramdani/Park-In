b50d990d0bd082b0bfe6db9fa69511bebcd73631
AldiRamdani <aldiramdani8@gmail.com>
2018-04-27 10:33:22 +0700

    Another PT

--- app/src/main/java/com/d3ifcool/park_in/PengembangActivity.java
+++ app/src/main/java/com/d3ifcool/park_in/PengembangActivity.java
@@ -6,13 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class PengembangActivity extends AppCompatActivity {
     TextView pengembang;
+    public static TextView txt_ver_baru;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengembang);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        txt_ver_baru = (TextView)findViewById(R.id.txt_ver_baru);
+        fetchData proses = new fetchData();
+        proses.execute();
         pengembang = (TextView)findViewById(R.id.pengembang);
         pengembang.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,7 +29,10 @@ public class PengembangActivity extends AppCompatActivity {
                 Intent i = new Intent(getApplicationContext(),DevActivity.class);
                 startActivity(i);
                 finish();
+
+
             }
         });
     }
+
 }
---
+++ app/src/main/java/com/d3ifcool/park_in/fetchData.java
@@ -0,0 +1,64 @@
+package com.d3ifcool.park_in;
+
+import android.os.AsyncTask;
+
+import com.d3ifcool.park_in.PengembangActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Aldir on 4/27/2018.
+ */
+
+public class fetchData extends AsyncTask<Void,Void,Void> {
+    String data;
+    String dataParsed;
+    String singelParsed;
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://api.myjson.com/bins/15wscn");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line="";
+            while (line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+            JSONArray JA = new JSONArray(data);
+            for (int i = 0; i < JA.length();i++){
+                JSONObject JO  = (JSONObject)JA.get(i);
+                singelParsed = "Version :"+JO.get("version");
+                dataParsed = dataParsed + singelParsed;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        PengembangActivity.txt_ver_baru.setText(this.data);
+    }
+}
